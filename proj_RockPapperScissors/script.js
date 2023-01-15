be611f800442eb384fbfd7e80a1eacc4bc54b496
turpija <turpija@gmail.com>
2023-01-15 15:46:24 +0100

    RPS - userSelection done, na grubo

--- proj_RockPapperScissors/script.js
+++ proj_RockPapperScissors/script.js
@@ -11,14 +11,45 @@ pokrećem funkc game
 
 */
 
-function getComputerChoice(){
-    let num = Math.floor(Math.random()*3)+1;
+function getComputerChoice() {
+    /*
+        nasumično izabire broj od 1 do 3 i dodjeljuje rock/paper/scissors
+    */
+    let num = Math.floor(Math.random() * 3) + 1;
     switch (num) {
-        case 1: return "paper"
-        case 2: return "rock"
-        case 3: return "scissors"
+        case 1:
+            return "paper";
+        case 2:
+            return "rock";
+        case 3:
+            return "scissors";
     }
 
 }
 
+function userSelection() {
+    /* 
+        -- DODAJ --
+        šta ako izabere nešto što ne postoji
+        dodaj while loop, prompt dok ne izabere točno
+    */
+
+    let userChoice = prompt('(R)ock", "(P)aper" or "(S)cissors"').toLowerCase();
+    
+    switch (userChoice) {
+        case "rock":
+        case "r":
+            return "rock";
+        case "paper":
+        case "p":
+            return "paper";
+        case "scissors":
+        case "s":
+            return "scissors";
+        default:
+            break;
+    }
+}
+
 console.log(getComputerChoice());
+console.log(userSelection());
