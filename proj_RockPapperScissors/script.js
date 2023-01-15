5f5c5653fb90abdcef48a87bfb7e585eff8d032d
turpija <turpija@gmail.com>
2023-01-15 15:32:16 +0100

    RPS: computeChoice funcition done

--- proj_RockPapperScissors/script.js
+++ proj_RockPapperScissors/script.js
@@ -1,1 +1,24 @@
 console.log("ROCK PAPPER SCISSORS");
+
+/*
+
+pokrećem funkc game
+    -> pokreće se f.playRound (uzima obje strane i zapisuje rezultat)
+        -> pokreće f.getComputerChoice (random izbor 1,2,3)
+        -> pokreće f.userSelection (prompt usera)
+        - loop 5 rundi        
+= tko je pobjednik, gotovo
+
+*/
+
+function getComputerChoice(){
+    let num = Math.floor(Math.random()*3)+1;
+    switch (num) {
+        case 1: return "paper"
+        case 2: return "rock"
+        case 3: return "scissors"
+    }
+
+}
+
+console.log(getComputerChoice());
